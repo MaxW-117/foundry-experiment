70e8ace3bb321f5d749fb3cd46e899831aff877f
Max <willmwatkins@gmail.com>
2023-09-15 17:10:19 -0600

    global vars

--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -5,3 +5,46 @@ MYTT.foobar = {
   'bas': 'MYTT.bas',
   'bar': 'MYTT.bar'
 };
+
+MYTT.stats = {
+  "spi": {
+    "name":"Spirit",
+    "description":""
+  },
+  "wis": {
+    "name":"Wisdom",
+    "description":""
+  },
+  "int": {
+    "name":"Intelligence",
+    "description":""
+  },
+  "per": {
+    "name":"Perception",
+    "description":""
+  },
+  "cha": {
+    "name":"Charisma",
+    "description":""
+  },
+  "vit": {
+    "name":"Vitality",
+    "description":""
+  },
+  "end": {
+    "name":"Endurance",
+    "description":""
+  },
+  "str": {
+    "name":"Strength",
+    "description":""
+  },
+  "dex": {
+    "name":"Dexterity",
+    "description":""
+  },
+  "agi": {
+    "name":"Agility",
+    "description":""
+  },
+};
