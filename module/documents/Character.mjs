f020da3b03870187e1c0c8521fa346c97bc55315
Max <willmwatkins@gmail.com>
2023-09-16 22:38:35 -0600

    resource bars

--- module/documents/Character.mjs
+++ module/documents/Character.mjs
@@ -22,7 +22,16 @@ export class Character extends ActorBase {
 
     static sheetCharacterData(context) {
         const data = context.data; 
+        data.health.max = Math.max(1, data.health.max)
         data.health.percentage = Math.floor(10000 * data.health.value / data.health.max)/100
+
+        data.showMana = data.mana.max > 10
+        data.mana.max = Math.max(1, data.mana.max)
+        data.mana.percentage = Math.floor(10000 * data.mana.value / data.mana.max)/100
+
+        data.showStamina = data.stamina.max > 10
+        data.stamina.max = Math.max(1, data.stamina.max)
+        data.stamina.percentage = Math.floor(10000 * data.stamina.value / data.stamina.max)/100
     }
 
     static sheetItemData(context) {
